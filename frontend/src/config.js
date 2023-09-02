bc9bbbaa34ab20e6e95056025f325dc18d262213
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-09-02 20:30:07 +0600

    added file config

---
+++ frontend/src/config.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+
+export const USERS = 'users';
+
+export const INFO_USERS = 'info-users/';
+
+export const AXIOS = axios.create();
